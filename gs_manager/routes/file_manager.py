36b3db0545a5456ea956dae8a9e969637d94c916
Shane Volpe <shanevolpe@gmail.com>
2024-07-02 10:38:49 -0400

    Added support to view and delete volume mounts.

--- gs_manager/routes/file_manager.py
+++ gs_manager/routes/file_manager.py
@@ -8,6 +8,6 @@ fm = Blueprint('file_manager', __name__)
 
 @fm.route('/files/upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
+    # if request.method == 'POST':
     # return redirect(url_for('server_config.upload'))
     return render_template('file_manager/upload.html', modules=[{'module_name': 'mod1', 'dir_location': 'dir1'}, {'module_name': 'mod2', 'dir_location': 'dir2'}])

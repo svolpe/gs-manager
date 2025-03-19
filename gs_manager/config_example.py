c5b28b052795de29de86ecaa3dd8e99796b2ce74
Shane Volpe <shanevolpe@gmail.com>
2025-03-19 17:50:05 -0400

    Added an example of setting the allowed CORS in the config file

--- gs_manager/config_example.py
+++ gs_manager/config_example.py
@@ -3,12 +3,13 @@ from os import environ, path
 
 basedir = path.abspath(path.dirname(__file__))
 
-
+#If TESTING is set to True then the DevelopmentConfig is used, if False then ProductionConfig is used
+TESTING=True
 class Config(object):
     """Base config."""
     SECRET_KEY = "dev"
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
     
     # SQLALCHEMY_ENGINE_OPTIONS lets you set connection options. The example below increases the timeout
     # in order to reduce write timeout errors
@@ -19,7 +20,8 @@ class ProductionConfig(Config):
     FLASK_ENV = "production"
     FLASK_DEBUG = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///gsmanager.sqlite"
-    GS_PATH_STORAGE = '/var/tmp/gs-manager/docker_storage'
+    GS_PATH_STORAGE = "/var/tmp/gs-manager/docker_storage"
+    CORS_ALLOWED_ORIGINS = "localhost:5000"
     SQLALCHEMY_POOL_SIZE = 50
 
 class DevelopmentConfig(Config):
@@ -27,5 +29,6 @@ class DevelopmentConfig(Config):
     FLASK_ENV = "development"
     FLASK_DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///gsmanager.sqlite"
-    GS_PATH_STORAGE = '/var/tmp/gs-manager/docker_storage'
+    GS_PATH_STORAGE = "/var/tmp/gs-manager/docker_storage"
+    CORS_ALLOWED_ORIGINS = "localhost:5000"
     SQLALCHEMY_POOL_SIZE = 50

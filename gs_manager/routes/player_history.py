4654d88b4f21648ea68aceefbc7a766ef2ae3a7f
Shane Volpe <shanevolpe@gmail.com>
2026-03-21 01:23:23 -0400

    Initial addition of player login summary table

--- gs_manager/routes/player_history.py
+++ gs_manager/routes/player_history.py
@@ -4,7 +4,7 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from .auth import login_required
-from sqlalchemy import (delete, insert)
+from sqlalchemy import delete, insert, func
 from ..extensions import db
 from ..models.server_nwn import PcActiveLog
 from sqlalchemy import null
@@ -13,17 +13,47 @@ from sqlalchemy import null
 ph = Blueprint('player_history', __name__)
 
 
-
 @ph.route('/player_history')
-def index():     
-    return render_template(
-        'player_history/index.html')
+def index():
+    return render_template('player_history/index.html')
+
+
+@ph.route('/player_history/summary/data')
+def summary_data():
+    rows = (
+        db.session.query(
+            PcActiveLog.player_name,
+            func.count(PcActiveLog.id).label('login_count')
+        )
+        .filter(PcActiveLog.logoff_time.isnot(None))
+        .group_by(PcActiveLog.player_name)
+        .all()
+    )
+    return {'data': [{'player_name': r.player_name, 'login_count': r.login_count} for r in rows]}
+
+
+@ph.route('/player_history/list')
+def list_view():
+    return render_template('player_history/list.html')
 
 
 @ph.route('/player_history/data')
 def data():
     query = PcActiveLog.query.filter(PcActiveLog.logoff_time.isnot(None))
-    data = {'data': [user.to_dict() for user in query]}
-    return data
+    return {'data': [user.to_dict() for user in query]}
+
+
+@ph.route('/player_history/player/<player_name>')
+def player_detail(player_name):
+    return render_template('player_history/player_detail.html', player_name=player_name)
+
+
+@ph.route('/player_history/player/<player_name>/data')
+def player_data(player_name):
+    query = PcActiveLog.query.filter(
+        PcActiveLog.logoff_time.isnot(None),
+        PcActiveLog.player_name == player_name
+    )
+    return {'data': [row.to_dict() for row in query]}
 
 
